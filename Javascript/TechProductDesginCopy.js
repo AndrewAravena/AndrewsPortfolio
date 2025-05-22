abba9fd8ad9893a04fb31027a1e75d342041c8e9
AndrewAravena <andrewaravenamunoz@gmail.com>
2025-05-23 10:47:23 +1200

    testing to see if the new photos work

--- Javascript/TechProductDesginCopy.js
+++ Javascript/TechProductDesginCopy.js
@@ -1,7 +1,22 @@
-
 const images_chan_fusion = [
-  'resources/chandiddy/chain.jpg', 'resources/chandiddy/ChainConnecttionchandiddy1.jpg', 'resources/chandiddy/chainshowhowworky.jpg', 'resources/chandiddy/chandiddylierhalfsecctionview.jpg', 'resources/chandiddy/chandiddylierlightoverall.jpg', 'resources/chandiddy/chandiddyligthcloseuporientation.jpg', 'resources/chandiddy/chandiddyligthview.jpg', 'resources/chandiddy/halfsectionofthechandiddylier.jpg'
-
+  "resources/chandiddy/chain.jpg",
+  "resources/chandiddy/ChainConnecttionchandiddy1.jpg",
+  "resources/chandiddy/chainshowhowworky.jpg",
+  "resources/chandiddy/chandiddylierhalfsecctionview.jpg",
+  "resources/chandiddy/chandiddylierlightoverall.jpg",
+  "resources/chandiddy/chandiddyligthcloseuporientation.jpg",
+  "resources/chandiddy/chandiddyligthview.jpg",
+  "resources/chandiddy/halfsectionofthechandiddylier.jpg",
+];
+const images_chan_phys = [
+  "resources/chandiddyphysproj/20250413_094842.jpg",
+  "resources/chandiddyphysproj/20250413_094847.jpg",
+  "resources/chandiddyphysproj/20250413_095147.jpg",
+  "resources/chandiddyphysproj/20250501_083243.jpg",
+  "resources/chandiddyphysproj/20250501_083248.jpg",
+  "resources/chandiddyphysproj/20250503_194435.jpg",
+  "resources/chandiddyphysproj/20250503_194438.jpg",
+  "resources/chandiddyphysproj/20250503_194445.jpg",
 ];
 
 class Carousel {
@@ -13,8 +28,9 @@ class Carousel {
     this.left_btn = left_btn;
     this.right_btn = right_btn;
 
-    this.slide_counter.innerHTML = `${this.current_image + 1}/${this.imgs_list.length
-      }`;
+    this.slide_counter.innerHTML = `${this.current_image + 1}/${
+      this.imgs_list.length
+    }`;
     this.left_btn.addEventListener("click", () => this.set_image(-1));
     this.right_btn.addEventListener("click", () => this.set_image(1));
   }
@@ -31,10 +47,19 @@ class Carousel {
 
     this.img.src = this.imgs_list[this.current_image];
 
-    this.slide_counter.innerHTML = `${this.current_image + 1}/${this.imgs_list.length
-      }`;
+    this.slide_counter.innerHTML = `${this.current_image + 1}/${
+      this.imgs_list.length
+    }`;
   }
 }
+
+carousel_chan = new Carousel(
+  document.getElementById("chan_phys_project_images"),
+  images_chan_phys,
+  document.getElementById("chan_phys_project_left"),
+  document.getElementById("chan_phys_project_right"),
+  document.getElementById("chan_phys_slide_int")
+);
 
 carousel_chan = new Carousel(
   document.getElementById("chan_fusion_project_images"),
--- python/ayyayaa.py
+++ python/ayyayaa.py
@@ -2,7 +2,7 @@ import os
 
 base_dir = os.path.abspath(".")
 
-os.chdir("resources/2025chandiddy")
+os.chdir("resources/chandiddyphysproj")
 
 images = []
 
@@ -10,6 +10,6 @@ for file in os.listdir("."):
     if file.lower().endswith(".jpg"):
         abs_path = os.path.abspath(file)
         rel_path = os.path.relpath(abs_path, base_dir)
-        images.append(rel_path.replace(' \ ', "/").replace("DegitaltechnologyPortfolio/", ""))
+        images.append(rel_path.replace(' \ ', "/").replace("andrewsportfolio/", ""))
 
 print(images)
